ce7f6860dae8f96ee0000bd0ed12ade324dcfe17
Wenxu Yu <13676459981@163.com>
2026-05-25 09:46:21 +0800

    初始化提交：招聘系统

---
+++ recruit-system/src/test/java/com/wits/recsys/RecruitSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wits.recsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
